4ae132ee3e19a456b9acf75897811e221be08d45
VladMSK1993 <sharvladrus@gmail.com>
2021-08-01 18:07:12 +0300

    ..

--- conftest.py
+++ conftest.py
@@ -3,28 +3,18 @@ from selenium import webdriver
 import pytest
 
 """Передача параметров через командную строку с помощью встроенной функции pytest_addoption и фикстуры request."""
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox") # default откроет поулмолчанию хромбраузер.
     parser.addoption('--language', action='store', default="en")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    if browser_name == "chrome":
-        options = Options() #Указываем язык браузера с помощью WebDriver, используя класс Options и метод add_experimental_option.
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language}) # Указываем что берётся язык пользователя.
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(firefox_profile=fp)
-    else:
-        print("Browser {} still is not implemented".format(browser_name))
+    options = Options()  # Указываем язык браузера с помощью WebDriver, используя класс Options и метод add_experimental_option.
+    print("\nstart chrome browser for test..")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})  # Указываем что берётся язык пользователя.
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,18 @@
-import time
+from .pages.main_page import MainPage
 
 
-def test_guest_see_button_text(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    time.sleep(5)
-    button = len(browser.find_elements_by_class_name("btn-primary")) # Измереям найден объект.
-    assert button > 0, "I can't find button" # На осовое того, что измерили объект выводим асерт, если значение < 0.
-    time.sleep(5)
+link = "http://selenium1py.pythonanywhere.com/"
 
-"""Для запуска тест через комнадную строку, необходимо использовать 
-    pytest -v --tb=line --language=en test_main_page.py 
-    '--tb=line' указывает, что нужно выводить только одну строку из лога каждого упавшего теста"""
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
