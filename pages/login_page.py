13d4b129f77adfb3fca0829beec533c1004e80b1
VladMSK1993 <sharvladrus@gmail.com>
2021-08-01 21:02:54 +0300

    Создал новый тест для страницы логина\регистрации, в котором 3 теста, для формы логина\формы регистрации и проверка урла на параметр логин.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,22 +1,23 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
-
     def should_be_login_url(self):
+        check_url = self.browser.current_url
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in check_url, "There's not the substring"
 
     def should_be_login_form(self):
-        check_login_form = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
-        # реализуйте проверку, что есть форма логина
-        assert True
+        try:
+            self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+            # реализуйте проверку, что есть форма логина
+        except():
+            assert self.browser.is_element_present(*LoginPageLocators.LOGIN_FORM), "There's not login form"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        try:
+            self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+            # реализуйте проверку, что есть форма регистрации на странице
+        except():
+            assert self.browser.is_element_present(*LoginPageLocators.REGISTER_FORM), "There's not register form"
