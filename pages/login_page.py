49cbce86ac8fb57626699e9892dffd490001277b
VladMSK1993 <sharvladrus@gmail.com>
2021-08-01 23:39:00 +0300

    Сделал кое-какие чистки кода. Сделал также переход между страницами по неявному принципу.

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,20 +4,20 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_url(self):
-        check_url = self.browser.current_url
         # реализуйте проверку на корректный url адрес
-        assert "login" in check_url, "There's not the substring"
+        current_url = self.browser.current_url
+        assert "login" in current_url, "There's not the substring"
 
     def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
         try:
             self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
-            # реализуйте проверку, что есть форма логина
         except():
             assert self.browser.is_element_present(*LoginPageLocators.LOGIN_FORM), "There's not login form"
 
     def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
         try:
             self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
-            # реализуйте проверку, что есть форма регистрации на странице
         except():
             assert self.browser.is_element_present(*LoginPageLocators.REGISTER_FORM), "There's not register form"
