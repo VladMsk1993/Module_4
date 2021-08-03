4eb55d0eac275d96d421bc96ffe9052832043a08
VladMSK1993 <sharvladrus@gmail.com>
2021-08-03 11:50:44 +0300

    Exchaned name of link, becouse it was a litlle bit incorrect.

--- test_product_page.py
+++ test_product_page.py
@@ -4,28 +4,28 @@ from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 
 
-link_basket = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link_product_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
 def test_guest_can_add_product_into_basket(browser):
-    link = link_basket
+    link = link_product_page
     page = ProductPage(browser, link)
     page.open()
     page.should_be_button_to_add_to_basket()
     quiz = BasePage(browser, link)
     quiz.solve_quiz_and_get_code()
-    time.sleep(2)
+    #time.sleep(300)
 
 
 def test_guest_see_product_page(browser):
-    link = link_basket
+    link = link_product_page
     page = ProductPage(browser, link)
     page.open()
     page.should_see_product_page()
 
 
 def test_guest_see_price(browser):
-    link = link_basket
+    link = link_product_page
     page = ProductPage(browser, link)
     page.open()
     page.should_be_button_to_add_to_basket()
@@ -35,7 +35,7 @@ def test_guest_see_price(browser):
 
 
 def test_guest_see_message_product_is_added(browser):
-    link = link_basket
+    link = link_product_page
     page = ProductPage(browser, link)
     page.open()
     page.should_be_button_to_add_to_basket()
