f67abdc48ab73c6c5167608d88b9474f166074cf
VladMSK1993 <sharvladrus@gmail.com>
2021-08-03 23:17:29 +0300

    Added two constructors for my negative tests.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
 import math
 
 
@@ -11,15 +14,30 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+    def open(self):
+        self.browser.get(self.url)
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except(NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
 
-    def open(self):
-        self.browser.get(self.url)
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
