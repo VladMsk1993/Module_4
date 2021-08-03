eb024ddaa9113c9e51d58d4982b601c84f671803
VladMSK1993 <sharvladrus@gmail.com>
2021-08-03 23:09:46 +0300

    Added one locator for 3 negative checks.

--- pages/locators.py
+++ pages/locators.py
@@ -12,9 +12,9 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
-    PRICE = (By.XPATH, "// *[ @ id = \"content_inner\"] / article / div[1] / div[2] / p[1]")
-    BASKET_PRICE = (By.XPATH, "// *[ @ id = \"messages\"] / div[3] / div / p[1] / strong")
-    PRODUCT = (By.XPATH, "//strong[text() = \"The shellcoder's handbook\"]")
+    PRICE = (By.XPATH, "//*[@id=\"content_inner\"]/article/div[1]/div[2]/p[1]")
+    BASKET_PRICE = (By.XPATH, "// *[@id =\"messages\"]/div[3]/div/p[1]/strong")
+    PRODUCT = (By.XPATH, "//strong[text()=\"The shellcoder's handbook\"]")
     MESSAGE_PRODUCT = (By.XPATH, "//*[@id=\"messages\"]/div[1]/div/strong")
+    SUCCESS_MESSAGE = (By.XPATH, "//*[@id=\"messages\"]/div[1]/div")
     PRODUCT = (By.XPATH, "//*[@id=\"content_inner\"]/article/div[1]/div[2]/h1")
-
