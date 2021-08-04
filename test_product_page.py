c985a756bea1cdedc9bcfacb1a51701e385de79c
VladMSK1993 <sharvladrus@gmail.com>
2021-08-04 10:21:35 +0300

    Added two test.

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,13 @@
 import time
-
 from .pages.locators import ProductPageLocators
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 import pytest
 
 
 link_product_page = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+link_product_page_2 = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
 
 
 def test_guest_can_add_product_into_basket(browser):
@@ -69,6 +70,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_message_disappeared()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = link_product_page_2
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = link_product_page_2
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
 
 
 @pytest.mark.parametrize('qparam', ["1","2","3","4","5","6",pytest.param("7", marks=pytest.mark.xfail),"8","9"])
