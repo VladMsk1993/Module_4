31a971af136a8483fd9b21f7d405140b54f8ba3c
VladMSK1993 <sharvladrus@gmail.com>
2021-08-04 13:39:57 +0300

    Added one test with two checks and exchanched name of variable to correct.

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,13 @@
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-link1 = "http://selenium1py.pythonanywhere.com/"
-link2 = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+link_main_page = "http://selenium1py.pythonanywhere.com/"
+link_login_page = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = link1
+    link = link_main_page
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -15,29 +16,37 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = link1
+    link = link_main_page
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link() # выполняет метод со страницы main_page (should_be_login_link).
 
 
 def test_guest_should_see_login_url(browser):
-    link = link2
+    link = link_login_page
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
 
 
 def test_guest_should_see_login_form(browser):
-    link = link2
+    link = link_login_page
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
 
 def test_guest_should_see_register_form(browser):
-    link = link2
+    link = link_login_page
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = link_main_page
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.basket_should_be_empty()
+    page.should_be_text_basket_is_empty()
