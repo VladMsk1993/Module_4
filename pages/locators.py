c074417e2cac37fb325e88bdec523d2a316b4cd2
VladMSK1993 <sharvladrus@gmail.com>
2021-08-05 16:27:32 +0300

    Added class for fixture

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    USER_LOGIN = (By.XPATH, "//*[@id=\"messages\"]/div")
+    ERROR_MESSAGE = (By.XPATH, "//*[@id=\"register_form\"]/div[1]")
 
 
 class ProductPageLocators():
@@ -22,8 +24,15 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # Добавили кортеж из двух значений.
     BASKET_BUTTON = (By.XPATH, "//*[@id=\"default\"]/header/div[1]/div/div[2]/span/a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
     EMPTY_BASKET = (By.CSS_SELECTOR, "#default > div.container-fluid.page > div > div.content")
     BASKET_ITEMS = (By.CSS_SELECTOR, "#basket_formset > div:nth-child(n)")
+
+class FixturePageLocators():
+    EMAIL = (By.XPATH, "//*[@id=\"id_registration-email\"]")
+    PASSWORD1 = (By.XPATH, "//*[@id=\"id_registration-password1\"]")
+    PASSWORD2 = (By.XPATH, "//*[@id=\"id_registration-password2\"]")
+    BUTTON = (By.XPATH, "//*[@id=\"register_form\"]/button")
