dbdb0227aa50d7432f05703bf1a25e5eab8d7908
VladMSK1993 <sharvladrus@gmail.com>
2021-08-05 15:59:26 +0300

    Added one class with mark for launch it by mark.

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+import pytest
 from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
@@ -6,20 +7,22 @@ from .pages.login_page import LoginPage
 link_main_page = "http://selenium1py.pythonanywhere.com/"
 link_login_page = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = link_main_page
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url) # Делаю инициализацию LoginPage
-    login_page.should_be_login_url() # Проверяю, что мы на нужной нам странице.
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = link_main_page
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url) # Делаю инициализацию LoginPage
+        login_page.should_be_login_url() # Проверяю, что мы на нужной нам странице.
 
-def test_guest_should_see_login_link(browser):
-    link = link_main_page
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link() # выполняет метод со страницы main_page (should_be_login_link).
+    def test_guest_should_see_login_link(self, browser):
+        link = link_main_page
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link() # выполняет метод со страницы main_page (should_be_login_link).
 
 
 def test_guest_should_see_login_url(browser):
