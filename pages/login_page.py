6adba9dc0a41c0e6852175822dc2fc253e94bab9
VladMSK1993 <sharvladrus@gmail.com>
2021-08-08 11:13:22 +0300

    Added function register_new user and tow assert.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,15 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import LoginPageLocators, FixturePageLocators
+import pytest
+import time
 
 
 class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         current_url = self.browser.current_url
@@ -21,3 +28,22 @@ class LoginPage(BasePage):
             self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
         except():
             assert self.browser.is_element_present(*LoginPageLocators.REGISTER_FORM), "There's not register form"
+
+    def should_be_user_login_message(self):
+        try:
+            self.browser.is_element_present(*LoginPageLocators.USER_LOGIN)
+        except():
+            assert self.browser.is_element_present(*LoginPageLocators.USER_LOGIN), "User isn't login the system."
+
+    def should_not_be_error_message(self):
+        try:
+            self.browser.is_not_element_present(*LoginPageLocators.ERROR_MESSAGE)
+        except():
+            assert self.browser.is_not_element_present(*LoginPageLocators.ERROR_MESSAGE), "There is some error message."
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*FixturePageLocators.EMAIL).send_keys(email)
+        self.browser.find_element(*FixturePageLocators.PASSWORD1).send_keys(password)
+        self.browser.find_element(*FixturePageLocators.PASSWORD2).send_keys(password)
+        self.browser.find_element(*FixturePageLocators.BUTTON).click()
+        time.sleep(5)
